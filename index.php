1da1d3c33b8463f94b1eb6d1e956d484801e8a85
PavelliOn <p.lukiyan@yandex.ru>
2024-02-01 19:49:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $list = [34,534,36,2345,65];
+        $Stas = ['age' => 20, 'car' => 'bmw', 'home' => 'butor-city'] ;
+        echo 'hello my nigga !';
+        echo '<br><hr>';
+        echo "$list[3]<br><hr>";
+        echo "$Stas[car]<br><hr>";
+        $matrix =[ 
+            [0,0,0,0,0],
+            [0,0,4,0,0],
+            [0,4,0,5,0],
+        ];
+        //$matrix [2][3] = 1;
+        echo $matrix[2][3];
+    ?>
+</body>
+</html>
